0f8d8de7615d9f2d290614d64008461d463433f9
Stig Voss <stig@stigvoss.dk>
2019-05-02 09:00:33 +0200

    Makes sidebar a component.

---
+++ src/TinfoilFeedReader/TinfoilFeedReader.Client/Shared/Components/CodeBehinds/SideBarComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinfoilFeedReader.Client.Shared.Components.CodeBehinds
+{
+    public class SideBarComponent : ComponentBase
+    {
+        bool collapseNavMenu = true;
+
+        protected string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        protected void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
