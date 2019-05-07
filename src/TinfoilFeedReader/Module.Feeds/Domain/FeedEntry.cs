57106fd07a229e5868813e5b8ab63a63da3773ec
Stig Voss <stig@stigvoss.dk>
2019-05-07 21:01:55 +0200

    Add simple time since publish.

--- src/TinfoilFeedReader/Module.Feeds/Domain/FeedEntry.cs
+++ src/TinfoilFeedReader/Module.Feeds/Domain/FeedEntry.cs
@@ -15,7 +15,29 @@ namespace Module.Feeds.Domain
 
         public DateTimeOffset PublishDate { get; set; }
 
+        public string SimpleTimeSincePublish()
+        {
+            var timeSince = DateTime.Now - PublishDate.LocalDateTime;
+
+            if (timeSince < TimeSpan.FromMinutes(1))
+            {
+                return $"{Math.Round(timeSince.TotalSeconds)}s";
+            }
+            else if (timeSince < TimeSpan.FromHours(1))
+            {
+                return $"{Math.Round(timeSince.TotalMinutes)}m";
+            }
+            else if (timeSince < TimeSpan.FromDays(1))
+            {
+                return $"{Math.Round(timeSince.TotalHours)}h";
+            }
+
+            return $"{Math.Round(timeSince.TotalDays)}d";
+        }
+
         public IEnumerable<string> Authors { get; set; }
+
+        public string Summary { get; set; }
 
         public static explicit operator FeedEntry(SyndicationItem item)
         {
@@ -24,7 +46,8 @@ namespace Module.Feeds.Domain
                 Title = item.Title?.Text,
                 Link = item.Links?.FirstOrDefault().Uri.AbsoluteUri,
                 PublishDate = item.PublishDate,
-                Authors = item.Authors?.Select(e => e.Name)
+                Authors = item.Authors?.Select(e => e.Name),
+                Summary = item.Summary?.Text
             };
         }
     }
