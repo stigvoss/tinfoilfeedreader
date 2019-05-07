040aca12191da86c4c2cb27b501d89abc8b0c718
Stig Voss <stig@stigvoss.dk>
2019-05-07 22:14:38 +0200

    Add service to handle content and read image URLs from feed content.

--- src/TinfoilFeedReader/Module.Feeds/Domain/FeedEntry.cs
+++ src/TinfoilFeedReader/Module.Feeds/Domain/FeedEntry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Module.Feeds.Domain.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
@@ -9,9 +10,9 @@ namespace Module.Feeds.Domain
     {
         public string Title { get; set; }
 
-        public string Link { get; set; }
+        public string SourceUrl { get; set; }
 
-        public string SourcePage => new Uri(Link).Host;
+        public string SourceDomain => new Uri(SourceUrl).Host;
 
         public DateTimeOffset PublishDate { get; set; }
 
@@ -39,16 +40,17 @@ namespace Module.Feeds.Domain
 
         public string Summary { get; set; }
 
+        public string ImageUrl { get; set; }
+
         public static explicit operator FeedEntry(SyndicationItem item)
-        {
-            return new FeedEntry
+            => new FeedEntry
             {
                 Title = item.Title?.Text,
-                Link = item.Links?.FirstOrDefault().Uri.AbsoluteUri,
+                SourceUrl = item.Links?.FirstOrDefault().Uri.AbsoluteUri,
                 PublishDate = item.PublishDate,
                 Authors = item.Authors?.Select(e => e.Name),
-                Summary = item.Summary?.Text
+                Summary = item.Summary?.Text,
+                ImageUrl = ContentService.ReadImage(item.Content)
             };
-        }
     }
 }
---
+++ src/TinfoilFeedReader/Module.Feeds/Domain/Services/ContentService.cs
@@ -0,0 +1,34 @@
+﻿using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using System.Text;
+
+namespace Module.Feeds.Domain.Services
+{
+    public class ContentService
+    {
+        public static string ReadImage(SyndicationContent content)
+        {
+            var textContent = content as TextSyndicationContent;
+
+            if (textContent is null)
+            {
+                return null;
+            }
+
+            if (textContent.Type?.ToLower() != "html")
+            {
+                return null;
+            }
+
+            var parser = new HtmlParser();
+
+            var document = parser.ParseDocument(textContent.Text);
+
+            var element = document.QuerySelector("img:first-of-type") as AngleSharp.Html.Dom.IHtmlImageElement;
+
+            return element?.Source;
+        }
+    }
+}
