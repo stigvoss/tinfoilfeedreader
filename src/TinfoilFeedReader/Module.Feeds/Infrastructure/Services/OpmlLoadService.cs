99e0c89544257a1a73ab86d8be00a26f551e6dad
Stig Voss <stig@stigvoss.dk>
2019-05-09 12:16:57 +0200

    Add OPML parsing.

---
+++ src/TinfoilFeedReader/Module.Feeds/Infrastructure/Services/OpmlLoadService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Module.Feeds.Domain;
+
+namespace Module.Feeds.Infrastructure.Services
+{
+    public class OpmlLoadService
+    {
+        public Feed LoadFeedFrom(string opml)
+        {
+            var document = XDocument.Parse(opml);
+
+            var root = document.Element("opml");
+            var body = root?.Element("body");
+            var outline = body?.Element("outline");
+            var title = outline?.Attribute("title");
+
+            if (title is null)
+            {
+                return null;
+            }
+
+            return new Feed
+            {
+                Name = title?.Value,
+                Sources = InspectOutlines(outline)
+            };
+        }
+
+        private ICollection<FeedSource> InspectOutlines(XElement outline)
+        {
+            var feedSources = new List<FeedSource>();
+
+            if (outline.HasElements)
+            {
+                var nestedOutlines = outline.Elements("outline");
+
+                foreach (var nestedOutline in nestedOutlines)
+                {
+                    var childrenSources = InspectOutlines(nestedOutline);
+                    feedSources.AddRange(childrenSources);
+                }
+            }
+            else
+            {
+                var type = outline.Attribute("type")?.Value;
+
+                var title = outline.Attribute("title")?.Value;
+                var url = outline.Attribute("xmlUrl")?.Value;
+
+                if (type == "rss" && title is object && url is object)
+                {
+                    var feedSource = new FeedSource
+                    {
+                        Name = title,
+                        Url = new Uri(url)
+                    };
+
+                    feedSources.Add(feedSource);
+                }
+            }
+
+            return feedSources;
+        }
+    }
+}
---
+++ tests/TinfoilFeedReader/Module.Feeds.Tests/A_OpmlLoadService.cs
@@ -0,0 +1,67 @@
+using Module.Feeds.Domain;
+using Module.Feeds.Infrastructure.Services;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module.Feeds.Tests
+{
+    public class A_OpmlLoadService
+    {
+        private const string TestOpml = "<opml version=\"1.1\"><body><outline text=\"YouTube-abonnementer\" title=\"YouTube-abonnementer\"><outline text=\"SmarterEveryDay\" title=\"SmarterEveryDay\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UC6107grRI4m0o2-emgoDnAA\" /><outline text=\"Veritasium\" title=\"Veritasium\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UCHnyfMqiRRG1u-2MsSQLbXA\" /><outline text=\"Vsauce\" title=\"Vsauce\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UC6nSFpj9HTCZ5t-N3Rm3-HA\" /><outline text=\"Tom Scott\" title=\"Tom Scott\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UCBa659QWEk1AI4Tg--mrJ2A\" /><outline text=\"SciShow\" title=\"SciShow\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UCZYTClx2T1of7BRZ86-8fow\" /><outline text=\"minutephysics\" title=\"minutephysics\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UCUHW94eEFW7hkUMVaZz4eDg\" /><outline text=\"CGP Grey\" title=\"CGP Grey\" type=\"rss\" xmlUrl=\"https://www.youtube.com/feeds/videos.xml?channel_id=UC2C_jShtL725hvbm1arSV9w\" /></outline></body></opml>";
+
+        private OpmlLoadService _opml;
+
+        [SetUp]
+        public void Setup()
+        {
+            _opml = new OpmlLoadService();
+        }
+
+        [Test]
+        public void Can_Read_Feed_Names()
+        {
+            string expectedFeedName = "YouTube-abonnementer";
+
+            Feed feed = _opml.LoadFeedFrom(TestOpml);
+
+            Assert.That(feed.Name, Is.EqualTo(expectedFeedName));
+        }
+
+        [Test]
+        public void Can_Read_FeedSources()
+        {
+            int expectedFeedCount = 7;
+
+            Feed feed = _opml.LoadFeedFrom(TestOpml);
+
+            Assert.That(feed.Sources.Count, Is.EqualTo(expectedFeedCount));
+        }
+
+        [Test]
+        public void Can_Read_FeedSource_Names()
+        {
+            Feed feed = _opml.LoadFeedFrom(TestOpml);
+
+            foreach (var feedSource in feed.Sources)
+            {
+                Assert.That(feedSource.Name, Is.Not.Null);
+                Assert.That(feedSource.Name, Is.Not.Empty);
+            }
+
+            Assert.That(feed.Sources.Select(fs => fs.Name), Contains.Item("SmarterEveryDay"));
+            Assert.That(feed.Sources.Select(fs => fs.Name), Contains.Item("CGP Grey"));
+        }
+
+        [Test]
+        public void Can_Read_FeedSource_Urls()
+        {
+            Feed feed = _opml.LoadFeedFrom(TestOpml);
+
+            foreach (var feedSource in feed.Sources)
+            {
+                Assert.That(feedSource.Url.Host, Is.EqualTo("www.youtube.com"));
+            }
+        }
+    }
+}
