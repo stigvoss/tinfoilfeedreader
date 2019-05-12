46e6daa8afbcd426389c49ca010bccd9321007b6
Stig Voss <stig@stigvoss.dk>
2019-05-12 12:44:44 +0200

    Add page to manage collections.

---
+++ src/TinfoilFeedReader/TinfoilFeedReader.Client/Pages/CodeBehinds/ManageFeedCollectionsComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Module.Feeds.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TinfoilFeedReader.Client.Pages.CodeBehinds
+{
+    public class ManageFeedCollectionsComponent : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Parameter]
+        protected Guid? FeedCollectionId { get; set; }
+
+        public FeedCollection Collection { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            Collection = await Http.GetJsonAsync<FeedCollection>($"api/feedcollections/{FeedCollectionId.Value}")
+                    .ConfigureAwait(false);
+        }
+    }
+}
